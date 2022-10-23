eada9d57e190741996551765c271c5410819691c
july000 <3636398752@qq.com>
2022-10-24 02:44:07 +0800

    1. Using class to save and update accuracy. 2. Define train function.

--- eval.py
+++ eval.py
@@ -13,4 +13,23 @@ def accuracy(output, label, topk=(1,)):
 
     return rtn
 
+# using class to save and update sccuracy.
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+    
+    def update(self, val, n=1):
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
+    
+    
+
+
+
     
